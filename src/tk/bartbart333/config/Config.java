5285f02eb7572b7340d95d1ebda1ea8745ae0496
bartbart333 <bartholdbos@hotmail.com>
2014-04-16 17:53:39 +0200

    Config add load()

--- src/tk/bartbart333/config/Config.java
+++ src/tk/bartbart333/config/Config.java
@@ -1,11 +1,15 @@
 package tk.bartbart333.config;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Config {
 	
 	private File file;
+	private Properties config = new Properties();
 	
 	public Config(String config){
 		file = new File(config + ".config");
@@ -16,6 +20,16 @@ public class Config {
 			}catch(IOException e){
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public void load(){
+		try{
+			config.load(new FileInputStream(file));
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
 		}
 	}
 }
