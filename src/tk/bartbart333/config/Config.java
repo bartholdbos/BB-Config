00e0596eacb2888753f0cb0cb6373f23888760dc
bartbart333 <bartholdbos@hotmail.com>
2014-04-16 17:51:35 +0200

    Config creates config file

--- src/tk/bartbart333/config/Config.java
+++ src/tk/bartbart333/config/Config.java
@@ -1,8 +1,21 @@
 package tk.bartbart333.config;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Config {
+	
+	private File file;
+	
+	public Config(String config){
+		file = new File(config + ".config");
 		
-	public Config(){
-		
+		if(!file.exists()){
+			try{
+				file.createNewFile();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
 }
